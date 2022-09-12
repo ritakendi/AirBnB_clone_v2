9c56e48df48fa74822829b5816438907c54d8469
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2022-09-13 01:01:56 +0300

    modified:   models/user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-""" An User class that inherits from BaseModel """
+""" 
+    Implementation of the user class which inherits from BaseModel 
+"""
 
+import imp
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
-    """ Inherits from BaseModel,
-    instantiate with empty string as values for attributes """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    """ 
+        Defination of the User class
+    """
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user",
+                          cascade="all, delete-orphan")
+    reviews = relationship("Review", backref="user",
+                           cascade="all, delete-orphan")
