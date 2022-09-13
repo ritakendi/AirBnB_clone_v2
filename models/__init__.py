da2dc4792e84ec9e2f87fc3aeef8eeb3ca380789
austineoduor <austineoduor57@gmail.com>
2022-09-13 15:06:11 +0300

    file

--- models/__init__.py
+++ models/__init__.py
@@ -11,10 +11,10 @@ from models.review import Review
 from os import environ
 
 if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    pass
    """ from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()"""
-        pass
 
 else:
     from models.engine.file_storage import FileStorage
