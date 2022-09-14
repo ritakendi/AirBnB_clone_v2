115c21d799cca0c230d825fc8825edff126b2932
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2022-09-14 18:41:05 +0300

    code changed

--- models/amenity.py
+++ models/amenity.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""
-Amenity module for HBNB project
-"""
+""" Amenity Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """
-    This class Represents Amenities for a place
-
-    Inherits the BaseModel and Base(from sqlachemy)and links to the mysql
-    table amenities.
+    """ Represent a the Amenities for a place
+    
+    Inherits the BaseModel and Base(from sqlalchemy) and links to the mysql table amenities
     Attributes:
-    __tablename__(str): name of the MYSQL table
-        name(sqlalchemy string): name of the City
+      __tablename__(str): name of the MySQL table
+      name(sqlalchemy String): name of the City
     """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity,
-                                   backref='amenities')
+    if models.storage_type == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+#        place_amenities = relationship("Place", secondary="place_amenity",
+#                                       viewonly=False)
+    else:
+        def __init__(self, name = ""):
+            """ if storage is not db (i.e. FileStorage) instantiate the values """
+            self.name = name
+            super().__init__()
