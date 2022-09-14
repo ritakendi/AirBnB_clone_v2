dc187940a314ace4164859fe2169569fdc1b08e5
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2022-09-14 18:53:32 +0300

    checked importation

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,7 @@
     Amenity module for HBNB project
 """
 
-from models import BaseModel, Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
