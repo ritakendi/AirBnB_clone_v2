a26f1030b400dce958c2f009b4e946ae19c7f11d
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2022-09-14 18:43:04 +0300

    modified:   models/amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
+"""
+Amenity module for HBNB project
+"""
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """ Represent a the Amenities for a place
-    
-    Inherits the BaseModel and Base(from sqlalchemy) and links to the mysql table amenities
-    Attributes:
-      __tablename__(str): name of the MySQL table
-      name(sqlalchemy String): name of the City
     """
-    if models.storage_type == "db":
-        __tablename__ = "amenities"
-        name = Column(String(128), nullable=False)
-#        place_amenities = relationship("Place", secondary="place_amenity",
-#                                       viewonly=False)
-    else:
-        def __init__(self, name = ""):
-            """ if storage is not db (i.e. FileStorage) instantiate the values """
-            self.name = name
-            super().__init__()
+    This class Represents Amenities for a place
+
+    Inherits the BaseModel and Base(from sqlachemy)and links to the mysql
+    table amenities.
+    Attributes:
+    __tablename__(str): name of the MYSQL table
+        name(sqlalchemy string): name of the City
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place_amenity,
+                                   backref='amenities')
