f41dd7ad3e1a93be58f0c9123e84d56ed180a7dc
AUSTINEODUOR <austineoduor57@gmail.com>
2022-09-14 05:34:45 -0700

    files

--- models/user.py
+++ models/user.py
@@ -16,8 +16,3 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=False)
     places = relationship("Place", backref="user", cascade="all, delete")
     reviews = relationship("Review", backref="user", cascade="all, delete")
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
